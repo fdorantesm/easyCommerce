b026c04162cc92dca9d76ab685329d7a84002366
Fernando D <fernando@dorant.es>
2019-08-10 15:47:33 -0500

    Routes autoimport and save stack into environment file

--- src/app/http/router.js
+++ src/app/http/router.js
@@ -1,23 +1,9 @@
 import Router from 'router';
-
-const APP_NAME = process.env.APP_NAME;
+import routes from 'routes';
 
 // eslint-disable-next-line new-cap
 const router = Router();
 
-// Routes
-import auth from 'routes/auth';
-import test from 'routes/test';
-import cart from 'routes/cart';
-import order from 'routes/order';
-
-router.get('/', async (req, res) => {
-  res.render('index', {app: APP_NAME});
-});
-
-router.use('/auth', auth);
-router.use('/test', test);
-router.use('/cart', cart);
-router.use('/order', order);
+router.use('/', routes);
 
 export default router;
---
+++ src/app/http/routes/index.js
@@ -0,0 +1,43 @@
+import Router from 'router';
+import path from 'path';
+import glob from 'glob';
+import CliTable from 'cli-table';
+import trimEnd from 'lodash/trimEnd';
+import fs from 'fs';
+
+// eslint-disable-next-line new-cap
+const router = Router();
+
+const ROOT_PATH = process.env.ROOT_PATH;
+const APP_PATH = process.env.APP_PATH;
+
+// eslint-disable-next-line no-unused-vars
+const table = new CliTable({
+  head: ['Method', 'Path'],
+  colWidths: [30, 50],
+});
+
+/**
+ * Load subrouters and log each route into file
+ */
+glob.sync(APP_PATH + '/http/routes/*.js').forEach((file) => {
+  if (file !== __filename) {
+    const routes = require(file).default;
+    const base = `/${path.basename(file).replace('.js', '')}`;
+    router.use(base, routes);
+    // eslint-disable-next-line max-len
+    routes.stack.filter((r) => r.route).map((r) => {
+      // eslint-disable-next-line max-len
+      Object.keys(r.route.methods).map((method) => {
+        // eslint-disable-next-line max-len
+        table.push([method.toUpperCase(), trimEnd(base.concat(r.route.path), '/')]);
+      });
+    });
+  }
+  // eslint-disable-next-line max-len
+  fs.writeFileSync(path.join(ROOT_PATH, '.env.routes'), table.toString().replace(/\x1b\[[0-9;]*m/g, ''));
+});
+
+console.log(table.toString());
+
+export default router;
