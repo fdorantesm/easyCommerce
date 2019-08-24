56af93d6c4b5e7f1fb7ad72804c7a1ec02063e85
Fernando Dorantes <fdorantesm@gmail.com>
2019-08-23 20:50:15 -0500

    Resolve max-line warns

--- src/app/http/routes/users.js
+++ src/app/http/routes/users.js
@@ -7,14 +7,20 @@ import UserMiddleware from '../middlewares/casbin/users';
 // eslint-disable-next-line new-cap
 const router = Router();
 
+// eslint-disable-next-line max-len
 router.get('/', AuthMiddlewares.authorization, UsersMiddlewares.canListUsers, UserController.getUsers);
 router.get('/:id', UserController.getUser);
 // router.post('/:id', UserController.updateUser);
 // router.delete('/:id', UserController.deleteUser);
+// eslint-disable-next-line max-len
 router.post('/', AuthMiddlewares.authorization, UsersMiddlewares.canCreateUser, validator, UserController.createUser);
+// eslint-disable-next-line max-len
 router.post('/:id/roles', AuthMiddlewares.authorization, UsersMiddlewares.canAddUserRole, validator, UserController.assignRole);
+// eslint-disable-next-line max-len
 router.delete('/:id/roles/:role', AuthMiddlewares.authorization, UsersMiddlewares.canRemoveUserRole, validator, UserController.revokeRole);
+// eslint-disable-next-line max-len
 router.delete('/:id/roles', AuthMiddlewares.authorization, UsersMiddlewares.canRemoveUserRole, UserController.revokeRoles);
+// eslint-disable-next-line max-len
 router.get('/:id/roles', AuthMiddlewares.authorization, UserMiddleware.canListUserRole, UserController.getRoles);
 
 export default router;
