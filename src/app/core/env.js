0a3b6ee01f93835bcb254ed8011728a28a90cab4
Fernando Dorantes <fdorantesm@gmail.com>
2019-08-24 02:13:54 -0500

    Fix src path calls in dist app

--- src/app/core/env.js
+++ src/app/core/env.js
@@ -2,18 +2,18 @@ import path from 'path';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-
 const NODE_ENV = process.env.NODE_ENV;
 const PWD = process.env.PWD;
 
 const $root = PWD;
-const SRC_PATH = path.join($root, 'src').split('/');
+const $dist = path.join(__dirname, '../..').split('/').pop();
 
+process.env.SRC_PATH = path.join($root, $dist === 'dist' ? 'dist' : 'src')
 process.env.ROOT_PATH = $root;
-process.env.SRC_PATH = SRC_PATH.join('/');
 process.env.STORAGE_PATH = path.join(process.env.SRC_PATH, 'storage');
-const envFile = getEnv(NODE_ENV);
+process.env.APP_PATH = path.join(process.env.SRC_PATH, 'app');
 
+const envFile = getEnv(NODE_ENV);
 const file = fs.existsSync(envFile) ? envFile : getEnv();
 
 if (!fs.existsSync(file)) {
@@ -23,8 +23,6 @@ if (!fs.existsSync(file)) {
 dotenv.config({
   path: file
 });
-
-process.env.APP_PATH = path.join(process.env.SRC_PATH, 'app');
 
 /**
  * Get env path
